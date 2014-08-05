8a8bea0250709ff51ca5162b7efd6aa785dbcba4
Brandon Wagner <bmwagner10@gmail.com>
2014-08-05 17:46:27 -0400

    setting spring binder (not using grails binder, it works as expected)

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -31,6 +31,8 @@ grails.mime.types = [ // the first one is the default format
     xml:           ['text/xml', 'application/xml']
 ]
 
+grails.databinding.useSpringBinder=true
+
 // URL Mapping Cache Max Size, defaults to 5000
 //grails.urlmapping.cache.maxsize = 1000
 
